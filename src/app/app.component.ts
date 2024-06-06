a070e8b59fe63cadc7f68969005c4f4b9972fe73
christophthonhauser <c.thonhauser97@gmail.com>
2024-06-06 19:06:21 +0200

    init image upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NgOptimizedImage } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, NgOptimizedImage, MatButton],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'cat-v-dog-ui';
+  selectedFile: File | null = null;
+  imageUrl: string | ArrayBuffer | null = null;
+
+  constructor(private _snackBar: MatSnackBar) {}
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+      };
+      reader.readAsDataURL(this.selectedFile);
+    }
+  }
+
+  onUpload() {
+    if (this.selectedFile) {
+      const formData = new FormData();
+      formData.append('image', this.selectedFile);
+      this.openSnackBar('Uploaded Image', 'Close');
+    }
+  }
 }
